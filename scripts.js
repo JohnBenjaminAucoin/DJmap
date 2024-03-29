bea2bd378436c6e124d6b2ab0d17501f99187298
Benjamin Aucoin <43827051+JohnBenjaminAucoin@users.noreply.github.com>
2024-03-29 02:17:27 -0400

    minimum viable product

--- scripts.js
+++ scripts.js
@@ -112,7 +112,7 @@ async function getAudioFeatures(playlist , callback) {
 }
 
 function buttonClick(){
-    
+    getAuth();
     getPlaylist(isolatePlaylistID(inputE.value), getAudioFeatures); 
 }
 
@@ -138,65 +138,116 @@ async function createChart(playlist, audio_features) {
         bpm:
     }
     edge
-{ 
+    { 
     from:
     to:
     type:
-}
+    }
     */
 
 
     let playlistId = isolatePlaylistID(inputE.value);
+
+    let nodesize = 100;
+    
+    let modelookup ={
+        1 : "Major",
+        0 : "Minor"
+    }
+    let keylookup ={
+        1:"C",
+        2:"C#",
+        3:"D",
+        4:"D#",
+        5:"E",
+        6:"F",
+        7:"F#",
+        8:"G",
+        9:"G#",
+        10:"A",
+        11:"A#",
+        12:"B",
+    }
+
 
     //create nodes
     console.log(await playlist);
     for (let songN in playlist) {
         chartData.nodes.push({
             id: `${playlist[songN].track.name}`,
-            key: `${audio_features[songN].key}`,
-            mode: `${audio_features[songN].mode}`,
-            bpm: `${Math.round(audio_features[songN].tempo)}`
+            key: parseInt(audio_features[songN].key) +1,
+            keyLetter: keylookup[parseInt(audio_features[songN].key) +1],
+            mode: modelookup[parseInt(audio_features[songN].mode)],
+            bpm: Math.round(audio_features[songN].tempo),
+            fill: {src: playlist[songN].track.album.images[0].url},
+            height: 40
         });
     }
+    
+
     console.log(chartData);
     //create Edges
     for (let i = 0; i < chartData.nodes.length; i++) {
         for (let j = i + 1; j < chartData.nodes.length; j++) {
             let flag = false;
             let type;
-            let keyDelta = chartData.nodes[i].key - chartData.nodes[i].key
-
-
-            if(chartData.nodes[i].key == chartData.nodes[j].key && chartData.nodes[i].mode == chartData.nodes[j].mode){
+            let thickness;
+            let keyDelta = Math.abs(chartData.nodes[i].key - chartData.nodes[j].key);
+            let sameMode = chartData.nodes[i].mode == chartData.nodes[j].mode;
+            let bpmDelta = Math.abs(chartData.nodes[i].bpm - chartData.nodes[j].bpm);
+            if(keyDelta == 0 && sameMode){
                 type = "Perfect Mix";
                 flag = true;
-            }else if(){
+                color="#FF2ECC";
+                thickness = 4;
+                
+            }else if(Math.min(keyDelta, 12-keyDelta) == 1 && sameMode){//min(|a−b|,10−|a−b|)
                 //1 away
                 type = "1 Away";
                 flag = true;
-
-            }else if(){
+                color="#FF2ECC";
+            }else if(keyDelta == 1 && !sameMode){
                 //diagonal mix
                 type = "Diagonal Mix";
                 flag = true;
-            }else if(){
+                color = "#A51080";
+            }else if(keyDelta == 0 && !sameMode){
                 //scale change
                 type = "Scale Change";
                 flag = true;
+                color="#3B0230";
             }
             if (flag){
                 chartData.edges.push({
                     from: chartData.nodes[i].id,
                     to: chartData.nodes[j].id,
-                    type: type
+                    mixtype: type,
+                    stroke:{
+                        color: color,
+                        thickness: thickness
+                    },
+                    bpmDif: bpmDelta
                 })
             }
 
         }
+        
     }
-
+   // https://coolors.co/ff2ecc-a51080-3b0230-eef0f2-202124
     var chart = anychart.graph(chartData);
+    chart.background().fill("#202124");
+    chart.nodes().tooltip().useHtml(true);
+   
 
+
+
+
+
+
+chart.nodes().tooltip().format(
+  `<span style='font-weight:bold'>{%id}</span><br> {%keyLetter} {%mode} {%bpm} bpm`
+);
+     chart.edges().tooltip().format("{%mixtype} \n {%bpmDif} bpm appart");
     // set the title
     chart.title("Mix Map");
 
